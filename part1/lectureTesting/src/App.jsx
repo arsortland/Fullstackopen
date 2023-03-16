a020f9f72bb919a120724b725be6705e4c03c50b
risinggaardsortla <arsortland@gmail.com>
2023-03-16 19:06:48 +0100

    adding changes to lecturetesting

--- part1/lectureTesting/src/App.jsx
+++ part1/lectureTesting/src/App.jsx
@@ -1,26 +1,16 @@
-const Hello = (props) => {
-  const name = props.name
-  const age = props.age
+import { useState } from 'react'
 
-  const bornYear = () => new Date().getFullYear() - age
+const App = () => {
+  const [ counter, setCounter ] = useState(0)
+
+  setTimeout(
+    () => setCounter(counter + 1),
+    10000
+  )
 
   return (
-    <div>
-      <p>Hello {name}, you are {age} years old</p>
-      <p>So you were probably born in {bornYear()}</p>
-    </div>
+    <div>{counter}</div>
   )
 }
 
-
-
-const App = () =>{
-  return(
-    <div>
-    <Hello name={"Sigrid"} age={35}/>
-    </div>
-
-  )
-}
-
-export default App;
+export default App
