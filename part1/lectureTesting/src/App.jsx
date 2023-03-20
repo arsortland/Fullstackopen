170ec670d2eddeca53777dbcf326f499d97b3f12
risinggaardsortla <arsortland@gmail.com>
2023-03-20 14:35:18 +0100

    Added state and eventhandler for incementing number by using buttons

--- part1/lectureTesting/src/App.jsx
+++ part1/lectureTesting/src/App.jsx
@@ -3,13 +3,21 @@ import { useState } from 'react'
 const App = () => {
   const [ counter, setCounter ] = useState(0)
 
-  setTimeout(
-    () => setCounter(counter + 1),
-    10000
-  )
+  // setTimeout(
+  //   () => setCounter(counter + 1),
+  //   10000
+  // )
+
+  const handleClick = () => setCounter (counter +1);
+  const resetClick = () => setCounter(0);
 
   return (
-    <div>{counter}</div>
+    <div>
+      <div>{counter}</div>
+      <button onClick={handleClick}>Add</button>
+      <button onClick={resetClick}>Reset</button>
+    </div>
+
   )
 }
 
