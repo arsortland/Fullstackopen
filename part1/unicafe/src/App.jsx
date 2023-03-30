8010726b5cf3e37a922fc7dab598f199bc81af29
risinggaardsortla <arsortland@gmail.com>
2023-03-30 19:22:30 +0200

    my attempt on the final application before reading through the exercises

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,14 +1,54 @@
 import { useState } from 'react'
 
+
+const Header = ({text}) => {
+  return <h1>{text}</h1>
+}
+
+const Button = ({funcCount, text}) => {
+  return <button onClick={funcCount}>{text}</button>
+}
+
+const HeaderParagraph = ({text}) =>{
+  return <h2>{text}</h2>
+}
+
+const Stats = ({text, count}) => {
+  return <p>{text} {count}</p>
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+ const goodPress = () =>{
+  const newGoodVal = good + 1
+  setGood(newGoodVal)
+ }
+
+ const neutralPress = () =>{
+  const newNeutralVal = neutral + 1
+  setNeutral(newNeutralVal)
+ }
+
+ const badPress = () =>{
+  const newBadVal = bad + 1
+  setBad(newBadVal)
+ }
+
+
   return (
     <div>
-      code here
+      <Header text= "Give Feedback"/>
+      <Button funcCount={goodPress} text="Good"/>
+      <Button funcCount={neutralPress} text="Neutral"/>
+      <Button funcCount={badPress} text="Bad"/>
+      <HeaderParagraph text="Statistics"/>
+      <Stats text="Good" count={good}/>
+      <Stats text="Neutral" count={neutral}/>
+      <Stats text="Bad" count={bad}/>
     </div>
   )
 }
